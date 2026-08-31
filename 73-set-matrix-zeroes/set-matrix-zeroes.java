66aa5aaa97ffbc067837c8f2b52da2a66c2f1091
ujjvalrpatel <155754427+ujjvalrpatel@users.noreply.github.com>
2026-09-01 00:08:34 +0530

    Time: 4 ms (11.85%) | Memory: 47.5 MB (80.54%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        Set<Integer> zeroRows = new HashSet<>();
+        Set<Integer> zeroCols = new HashSet<>();
+        
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    zeroRows.add(i);
+                    zeroCols.add(j);
+                }
+            }
+        }
+        
+    
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (zeroRows.contains(i) || zeroCols.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
